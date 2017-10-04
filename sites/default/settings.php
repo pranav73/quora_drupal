244a0888d1799dd8a9a882ef89687870c21ed600
Pranav73 <pranavaeer73@gmail.com>
2017-10-04 16:43:08 +0530

    modify settings.php and gitignore it

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -290,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'KAr0r9hnS9vQo-scrXz2Qt_8vJKS1A2VwSUhyVcRF7w';
+$drupal_hash_salt = 'HZWB1JgKJAufA_mPKa6deLN4z8OaqXHCk0A_TKhTTK0';
 
 /**
  * Base URL (optional).
