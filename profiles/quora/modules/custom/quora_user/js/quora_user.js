c57a3e10acb807e395ddb6b254f044107b9f9e5a
Pranav73 <pranavaeer73@gmail.com>
2017-10-10 16:23:22 +0530

    Added image to set logo

--- profiles/quora/modules/custom/quora_user/js/quora_user.js
+++ profiles/quora/modules/custom/quora_user/js/quora_user.js
@@ -1,20 +1,20 @@
-(function ($) {
-
-  Drupal.behaviors.quorauserlogin = {
-    attach: function (context) {
-      // Hide the register by email block by default
-      $('.pane-formblock-user-register').hide();
-
-      // Toggel login by email and social login box.
-      $("#show-login-email").click(function() {
-        $('.pane-formblock-user-register').show();
-        $('.pane-hybridauth-hybridauth,.pane-custom.pane-3').hide();
-      });
-
-      $("#show-login-social").click(function() {
-        $('.pane-formblock-user-register').toggle();
-        $('.pane-hybridauth-hybridauth,.pane-custom.pane-3').toggle();
-      });
-    }
-  };
-})(jQuery);
+// (function ($) {
+//
+//   Drupal.behaviors.quorauserlogin = {
+//     attach: function (context) {
+//       // Hide the register by email block by default
+//       $('.pane-formblock-user-register').hide();
+//
+//       // Toggel login by email and social login box.
+//       $("#show-login-email").click(function() {
+//         $('.pane-formblock-user-register').show();
+//         $('.pane-hybridauth-hybridauth,.pane-custom.pane-3').hide();
+//       });
+//
+//       $("#show-login-social").click(function() {
+//         $('.pane-formblock-user-register').toggle();
+//         $('.pane-hybridauth-hybridauth,.pane-custom.pane-3').toggle();
+//       });
+//     }
+//   };
+// })(jQuery);
